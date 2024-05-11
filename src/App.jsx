ecc7414aa4b857ee131e9af84e3bdbfaf4563697
sunil2002k <kunwarsunil093@gmail.com>
2024-05-11 19:45:29 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+ import React from 'react'
+import Navbar from './components/Navbar'
+import Herosection from './components/Herosection'
+import FeatureSection from './components/FeatureSection'
+ import Workflow from './components/Workflow'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footers from './components/Footers'
+ 
+ 
+ function App() {
+   return (
+      <>
+      <Navbar/>
+      <Herosection/>
+      <FeatureSection/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footers/>
+      </>
+   )
+ }
+ 
+ export default App
